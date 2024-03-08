a2f128abc2e3eacfe4a2e5db0fed56f047b02a6b
Natannan <natannanz16@gmail.com>
2024-03-09 00:22:12 +0300

    Modify Poster.jsx

--- src/Components/Poster.jsx
+++ src/Components/Poster.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import logo from '../images/logo.png'
 
 function Poster(props){
     return(
         <div className="movie-card">
         <label>{props.quality}</label>
         <img className="movie-img" src={props.img} alt="movie" />
+        <img className="play-logo" src={logo} alt="logo"></img>
         <span>{props.title}</span>
         </div>
     )
